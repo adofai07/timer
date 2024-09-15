1819f1e68b245ac209fcce3f714cdd2eadfe32e7
adofai07 <chldmsgh695@gmail.com>
2024-09-15 13:22:08 +0900

    asdf

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from type import Time, Timer, Blocker
 if __name__ == "__main__":
     t = Timer(
         # Block until 10:30 am (uses 24h format)
-        Blocker(hour=10, minute=30),
+        # Blocker(hour=10, minute=30),
 
+        Time("work"      , minute=50),
+        Time("break"     , minute=5 ),
+        Time("work"      , minute=50),
+        Time("break"     , minute=5 ),
+        Time("work"      , minute=50),
+        Time("break"     , minute=5 ),
+        Time("work"      , minute=50),
+        Time("long break", minute=25),
         Time("work"      , minute=50),
         Time("break"     , minute=5 ),
         Time("work"      , minute=50),
